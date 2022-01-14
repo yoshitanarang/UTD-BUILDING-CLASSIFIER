01caee00910d9485ffa36aaa08049e8c194bdd84
Nehanth <nehanthnarendrula102>
2022-01-14 17:53:40 -0600

    Initial Commit

---
+++ Main View/MainViewController+PhotoPicker.swift
@@ -0,0 +1,37 @@
+import PhotosUI
+
+extension MainViewController: PHPickerViewControllerDelegate {
+    /// Creates a controller that gives the user a view they can use to select a photo from the device's library.
+    var photoPicker: PHPickerViewController {
+        var config = PHPickerConfiguration()
+        config.selectionLimit = 1
+        config.filter = PHPickerFilter.images
+
+        let photoPicker = PHPickerViewController(configuration: config)
+        photoPicker.delegate = self
+
+        return photoPicker
+    }
+
+  
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: false)
+
+        guard let result = results.first else {
+            return
+        }
+
+        result.itemProvider.loadObject(ofClass: UIImage.self) { object, error in
+            if let error = error {
+                print("Photo picker error: \(error)")
+                return
+            }
+
+            guard let photo = object as? UIImage else {
+                fatalError("The Photo Picker's image isn't a/n \(UIImage.self) instance.")
+            }
+
+            self.userSelectedPhoto(photo)
+        }
+    }
+}
---
+++ Main View/MainViewController.swift
@@ -0,0 +1,109 @@
+import UIKit
+
+class MainViewController: UIViewController {
+    var firstRun = true
+
+    let imagePredictor = ImagePredictor()
+
+    let predictionsToShow = 2
+
+    @IBOutlet weak var startupPrompts: UIStackView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var predictionLabel: UILabel!
+}
+
+extension MainViewController {
+    
+    
+            @IBAction func singleTap() {
+
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            present(photoPicker, animated: false)
+            return
+        }
+
+        present(cameraPicker, animated: false)
+    }
+
+
+    @IBAction func doubleTap() {
+        present(photoPicker, animated: false)
+    }
+}
+
+extension MainViewController {
+    
+    func updateImage(_ image: UIImage) {
+        DispatchQueue.main.async {
+            self.imageView.image = image
+        }
+    }
+
+    
+    func updatePredictionLabel(_ message: String) {
+        DispatchQueue.main.async {
+            self.predictionLabel.text = message
+        }
+
+        if firstRun {
+            DispatchQueue.main.async {
+                self.firstRun = false
+                self.predictionLabel.superview?.isHidden = false
+                self.startupPrompts.isHidden = true
+            }
+        }
+    }
+   
+    func userSelectedPhoto(_ photo: UIImage) {
+        updateImage(photo)
+        updatePredictionLabel("Making predictions for the photo...")
+
+        DispatchQueue.global(qos: .userInitiated).async {
+            self.classifyImage(photo)
+        }
+    }
+
+}
+
+extension MainViewController {
+    
+    
+    private func classifyImage(_ image: UIImage) {
+        do {
+            try self.imagePredictor.makePredictions(for: image,
+                                                    completionHandler: imagePredictionHandler)
+        } catch {
+            print("Vision was unable to make a prediction...\n\n\(error.localizedDescription)")
+        }
+    }
+
+   
+    private func imagePredictionHandler(_ predictions: [ImagePredictor.Prediction]?) {
+        guard let predictions = predictions else {
+            updatePredictionLabel("No predictions. (Check console log.)")
+            return
+        }
+
+        let formattedPredictions = formatPredictions(predictions)
+
+        let predictionString = formattedPredictions.joined(separator: "\n")
+        updatePredictionLabel(predictionString)
+    }
+
+   
+    private func formatPredictions(_ predictions: [ImagePredictor.Prediction]) -> [String] {
+
+        let topPredictions: [String] = predictions.prefix(predictionsToShow).map { prediction in
+            var name = prediction.classification
+
+
+            if let firstComma = name.firstIndex(of: ",") {
+                name = String(name.prefix(upTo: firstComma))
+            }
+
+            return "\(name) - \(prediction.confidencePercentage)%"
+        }
+
+        return topPredictions
+    }
+}
